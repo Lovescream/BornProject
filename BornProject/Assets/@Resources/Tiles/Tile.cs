34f2cd06a34e5c904b5ed33ce00f5b3092c7e202
asd <114643897+Lovescream@users.noreply.github.com>
2024-01-27 22:14:31 +0900

    1

---
+++ BornProject/Assets/@Resources/Tiles/Tile.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting;
+
+namespace Map {
+
+    public abstract class Tile : ScriptableObject {
+
+
+
+    }
+}
