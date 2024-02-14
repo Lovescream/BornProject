ac4550b642b29b04b7d54d30f59e85d409482f73
Jya0w0 <jya0w0@gmail.com>
2024-02-14 16:41:07 +0900

    [Add] Animation
    Attack, Chase(Walk), Hit Animation 추가(Hit Animation은 임시)

--- BornProject/Assets/@Scripts/Models/Creatures/Creature.cs
+++ BornProject/Assets/@Scripts/Models/Creatures/Creature.cs
@@ -153,6 +153,7 @@ public class Creature : Entity {
     private void OnEnteredAttack()
     {
         _animator.SetTrigger(AnimatorParameterHash_Attack); // TODO: 공격 애니메이션 추가!!
+        Debug.Log("공격 애니메이션 재생");
     }
     private void OnEnteredDead() {
         _collider.enabled = false;
