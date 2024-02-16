96f50aa39c116ff9dcb664faf52ec66712a21da6
민현영 <alsgsudud077@naver.com>
2024-02-16 23:16:41 +0900

    [Fix]UI_Scene_Title

--- BornProject/Assets/@Scripts/UI/SceneUI/UI_Scene_Title.cs
+++ BornProject/Assets/@Scripts/UI/SceneUI/UI_Scene_Title.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class UI_Scene_Title : UI_Scene {
 
@@ -12,7 +13,7 @@ public class UI_Scene_Title : UI_Scene {
 
     enum Images{
 
-        ImageTitle
+        imageTitle
     }
 
     enum Buttons{
@@ -26,15 +27,35 @@ public class UI_Scene_Title : UI_Scene {
 
     enum Texts{
         textTitle,
-        textNewGame,
-        textContinue,
-        textController,
-        textExit
+        textxit
     }
 
     #endregion
 
-   
+    void Start()
+    {
+        Initialize();
+    }
 
-   
+    public override bool Initialize()
+    {
+        if (!base.Initialize()) return false;
+
+        BindImage(typeof(Images));
+        BindButton(typeof(Buttons));
+        BindText(typeof(Texts));
+
+        
+        GetButton((int)Buttons.btnNewGame).onClick.AddListener(OnBtnNewGame);
+
+        return true;
+    }
+
+    private void OnBtnNewGame() {
+
+        Main.UI.CloseAllPopup();
+        //Main.Scene.LoadScene("TestScene");
+    }
+
+
 }
