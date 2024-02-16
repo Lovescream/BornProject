b1dc10b6e8093b6725ea9485a200b40a1d353eea
Jya0w0 <jya0w0@gmail.com>
2024-02-16 20:57:27 +0900

    HELP
    도움 ! !  !

--- BornProject/Assets/@Scripts/Models/Creatures/Creature.cs
+++ BornProject/Assets/@Scripts/Models/Creatures/Creature.cs
@@ -26,6 +26,10 @@ public class Creature : Entity {
         get => _hp;
         set {
             if (_hp == value) return;
+            if (this.GetComponent<Player>() != null)
+            {
+                Debug.Log($"Player의 Hp를 {value}로 설정합니다.");
+            }
             if (value <= 0)
             {
                 if (State.Current != CreatureState.Dead)
@@ -178,6 +182,7 @@ public class Creature : Entity {
 
     public virtual void OnHit(IHitCollider attacker) {
         HitInfo hitInfo = attacker.HitInfo;
+        if (this.GetComponent<Enemy>() != null) Debug.Log($"곰이 {hitInfo.Damage} 피해를 입엇다");
         Debug.Log($"{hitInfo.Damage}의 피해를 입었따. 죽어라 - !");
         Hp -= hitInfo.Damage;
 
