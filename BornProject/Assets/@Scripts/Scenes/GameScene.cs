9339abe4986dc7f398cab8dbc90d722571e1f2c0
asd <114643897+Lovescream@users.noreply.github.com>
2024-02-21 14:43:05 +0900

    [Fix] GameOver, UI

--- BornProject/Assets/@Scripts/Scenes/GameScene.cs
+++ BornProject/Assets/@Scripts/Scenes/GameScene.cs
@@ -20,6 +20,7 @@ public class GameScene : BaseScene {
 
         // #3. UI 생성.
         SceneUI = Main.UI.OpenSceneUI<UI_Scene_Game>();
+        Main.UI.Clear();
 
         // #4. Skill 체크.
         Main.Skill.Reinitialize();
