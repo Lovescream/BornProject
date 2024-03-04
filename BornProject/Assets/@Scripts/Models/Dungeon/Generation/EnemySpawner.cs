b1f1889f79c986b30c32220d9e411ed821544dc7
민현영 <alsgsudud077@naver.com>
2024-03-04 15:04:37 +0900

    [Add] MushroomBoss

--- BornProject/Assets/@Scripts/Models/Dungeon/Generation/EnemySpawner.cs
+++ BornProject/Assets/@Scripts/Models/Dungeon/Generation/EnemySpawner.cs
@@ -2,75 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawner : MonoBehaviour {
+public class EnemySpawner : MonoBehaviour
+{
 
-    //#region Inspector
+    #region Inspector
 
-    ////[SerializeField]
-    ////private string enemyKey;
+    [SerializeField]
+    private string enemyKey;
 
-    //#endregion
+    #endregion
 
-    //#region Properties
+    #region Properties
 
-    //public string Key => enemyKey;
+    public string Key => enemyKey;
 
-    #region Fields
-
-    public string Key;
-    private int case1Counter = 0;
-    private string case2Selection = null;
     public Vector2 Position => this.transform.position;
 
     #endregion
-    private void Awake()
-    {
-        SpawnEnemyType(Main.Quest.ClearStageCount);
-    }
-    public void SpawnEnemyType(int stage)
-    {
-
-        System.Random random = new System.Random();
-        double randomNumber;
-        
-        switch (stage)
-        {
-            case 0:
-                randomNumber = random.NextDouble(); 
-                Key = randomNumber < 0.67 ? "Beatle" : "Snake";
-                break;
-            case 1:
-                Key = random.Next(2) == 0 ? "Snake" : "Wolf";
-                break;
-            case 2:
-                randomNumber = random.NextDouble();
-                if (randomNumber < 0.20)
-                    Key = "Snake";
-                else if (randomNumber < 0.50)
-                    Key = "Wolf";
-                else
-                    Key = "Bear";
-                break;
-            case 3:
-                randomNumber = random.NextDouble();
-                if (randomNumber < 0.20)
-                    Key = "Snake";
-                else if (randomNumber < 0.50)
-                    Key = "Wolf";
-                else
-                    Key = "Bear";
-                break;
-            case 4:
-                randomNumber = random.NextDouble();
-                if (randomNumber < 0.20)
-                    Key = "Snake";
-                else if (randomNumber < 0.50)
-                    Key = "Wolf";
-                else
-                    Key = "Bear";
-                break;
-
-        }
-    }
 
 }
