7389c87837347de545a179cb9b3cb54659e1c9ff
asd <114643897+Lovescream@users.noreply.github.com>
2024-03-06 17:37:31 +0900

    [Refactoring] ObjectManager

--- BornProject/Assets/@Scripts/Managers/ObjectManager.cs
+++ BornProject/Assets/@Scripts/Managers/ObjectManager.cs
@@ -1,53 +1,71 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class ObjectManager {
     public Player Player { get; private set; }
     public List<Enemy> Enemies { get; private set; } = new();
     public HashSet<HitCollider> HitColliders { get; private set; } = new();
 
+    #region General
+
     public void Clear() {
         Player = null;
         Enemies.Clear();
         HitColliders.Clear();
     }
+    private GameObject Spawn(string prefabKey, string defaultKey, bool pooling) {
+        if (string.IsNullOrEmpty(prefabKey)) {
+            if (string.IsNullOrEmpty(defaultKey)) {
+                Debug.LogError($"[ObjectManager] Spawn({prefabKey}, {defaultKey}): Spawn Failed. The key is null or empty.");
+                return null;
+            }
+            return Main.Resource.Instantiate(defaultKey, pooling: pooling);
+        }
+        if (Main.Resource.IsExist<GameObject>(prefabKey))
+            return Main.Resource.Instantiate(prefabKey, pooling: pooling);
+        if (string.IsNullOrEmpty(defaultKey)) {
+            Debug.LogError($"[ObjectManager] Spawn({prefabKey}, {defaultKey}): Spawn Failed. The key is null or empty.");
+            return null;
+        }
+        if (Main.Resource.IsExist<GameObject>(defaultKey))
+            return Main.Resource.Instantiate(defaultKey, pooling: pooling);
+
+        Debug.LogError($"[ObjectManager] Spawn({prefabKey}, {defaultKey}): Spawn Failed. Not found prefab with key.");
+        return null;
+    }
+    private void Despawn<T>(T obj) where T : Entity {
+        Main.Resource.Destroy(obj.gameObject);
+    }
+
+    #endregion
+
+    #region Spawn Entity
 
     public Player SpawnPlayer(string key, Vector2 position) {
-        Player = Spawn<Player>(position);
+        Player = Main.Resource.Instantiate("Player", pooling: false).GetComponent<Player>();
+        Player.transform.position = position;
         Player.SetInfo(Main.Data.Creatures[key]);
         return Player;
     }
     public Enemy SpawnEnemy(string key, Vector2 position) {
-        if (string.IsNullOrEmpty(key)) return null;
-        GameObject obj = Main.Resource.IsExist<GameObject>(key) ? Main.Resource.Instantiate($"{key}", pooling: true) : Main.Resource.Instantiate("Enemy", pooling: true);
-        //GameObject obj = Main.Resource.IsExistPrefab(key) ? Main.Resource.Instantiate($"{key}", pooling: true) : Main.Resource.Instantiate("Enemy", pooling: true);
+        GameObject obj = Spawn(key, "Enemy", true);
+        if (obj == null) {
+            Debug.LogError($"[ObjectManager] SpawnEnemy({key}, {position}): Spawn Failed.");
+            return null;
+        }
         obj.transform.position = position;
+
         Enemy enemy = obj.GetComponent<Enemy>();
         Enemies.Add(enemy);
         enemy.SetInfo(Main.Data.Creatures[key]);
         return enemy;
     }
-    
-    public void DespawnPlayer(Player player = null) {
-        if (player == null) player = Player;
-        if (player == null) return;
-        Player = null;
-        Despawn(player);
-    }
-    public void DespawnEnemy(Enemy enemy) {
-        Enemies.Remove(enemy);
-        Despawn(enemy);
-    }
     public HitCollider SpawnHitCollider(string key, string prefabKey, HitColliderInfo info, HitInfo hitInfo) {
-        if (string.IsNullOrEmpty(prefabKey)) {
-            Debug.LogError($"[ObjectManager] SpawnHitCollider({prefabKey}): Spawn Failed. the key is null or empty.");
+        GameObject obj = Spawn(prefabKey, "HitCollider_Base", true);
+        if (obj == null) {
+            Debug.LogError($"[ObjectManager] SpawnHitCollider({key}, {prefabKey}): Spawn Failed.");
             return null;
         }
-        //GameObject obj = Main.Resource.Instantiate(Main.Resource.IsExistPrefab(prefabKey) ? prefabKey : "HitCollider_Base", pooling: true);
-        GameObject obj = Main.Resource.Instantiate(Main.Resource.IsExist<GameObject>(prefabKey) ? prefabKey : "HitCollider_Base", pooling: true);
         obj.transform.position = Vector2.zero;
 
         HitCollider hitCollider = obj.GetComponent<HitCollider>();
@@ -55,10 +73,27 @@ public class ObjectManager {
         HitColliders.Add(hitCollider);
         return hitCollider;
     }
+
+    #endregion
+
+    #region Despawn Entity
+
+    public void DespawnPlayer() {
+        Despawn(Player);
+        Player = null;
+    }
+    public void DespawnEnemy(Enemy enemy) {
+        Enemies.Remove(enemy);
+        Despawn(enemy);
+    }
     public void DespawnHitCollider(HitCollider hitCollider) {
         HitColliders.Remove(hitCollider);
         Despawn(hitCollider);
     }
+
+    #endregion
+
+    #region Spawn InGameUI
 
     public void ShowDamageText(Vector2 position, float damage) {
         GameObject obj = Main.Resource.Instantiate("DamageText", pooling: true);
@@ -72,39 +107,6 @@ public class ObjectManager {
         bar.SetInfo(creature, hp, from);
     }
 
-    private T Spawn<T>(Vector2 position) where T : Entity {
-        Type type = typeof(T);
+    #endregion
 
-        string prefabName = null;
-        while (type != null) {
-            prefabName = type.Name;
-            //if (Main.Resource.LoadPrefab(prefabName) != null) break;
-            if (Main.Resource.IsExist<GameObject>(prefabName)) break;
-            type = type.BaseType;
-        }
-        if (string.IsNullOrEmpty(prefabName)) prefabName = "Thing";
-
-        GameObject obj = Main.Resource.Instantiate($"{prefabName}", pooling: true);
-        obj.transform.position = position;
-
-        return obj.GetOrAddComponent<T>();
-    }
-    private T Spawn<T>(string key, Vector2 position) where T : Entity {
-        if (string.IsNullOrEmpty(key)) {
-            Debug.LogError($"[ObjectManager] Spawn<{typeof(T).Name}>({key}, {position}): Spawn Failed. the key is null or empty.");
-            return null;
-        }
-        GameObject obj = Main.Resource.Instantiate(key, pooling: true);
-        if (obj == null) {
-            Debug.LogError($"[ObjectManager] Spawn<{typeof(T).Name}>({key}, {position}): Spawn Failed. Failed to load prefab.");
-            return null;
-        }
-        obj.transform.position = position;
-
-        return obj.GetComponent<T>();
-    }
-
-    private void Despawn<T>(T obj) where T : Entity {
-        Main.Resource.Destroy(obj.gameObject);
-    }
 }
--- BornProject/Assets/@Scripts/Managers/ResourceManager.cs
+++ BornProject/Assets/@Scripts/Managers/ResourceManager.cs
@@ -2,52 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using ZerolizeDungeon;
 using Object = UnityEngine.Object;
 public class ResourceManager {
-    //private Dictionary<string, Sprite> _sprites = new();
-    //private Dictionary<string, GameObject> _prefabs = new();
-    //private Dictionary<string, TextAsset> _jsonData = new();
-    //private Dictionary<string, RuntimeAnimatorController> _animControllers = new();
-    //private Dictionary<string, ZerolizeDungeon.Room> _rooms = new();
-    //private Dictionary<string, AudioClip> _clips = new();
     private Dictionary<Type, Dictionary<string, Object>> _resources = new();
 
     private bool _isInitialized;
     public void Initialize() {
-        //if (_isInitialized) return;
-        //_isInitialized = true;
-        //Sprite[] sprites = Resources.LoadAll<Sprite>("Sprites"); // TODO: 경로 지정.
-        //foreach (Sprite sprite in sprites) {
-        //    _sprites.Add(sprite.name, sprite);
-        //}
-
-        //GameObject[] objs = Resources.LoadAll<GameObject>("Prefabs");
-        //foreach (GameObject obj in objs) {
-        //    _prefabs.Add(obj.name, obj);
-        //}
-
-        //TextAsset[] texts = Resources.LoadAll<TextAsset>("JsonData");
-        //foreach (TextAsset t in texts) {
-        //    _jsonData.Add(t.name, t);
-        //}
-
-        //RuntimeAnimatorController[] controllers = Resources.LoadAll<RuntimeAnimatorController>("Animations");
-        //foreach (RuntimeAnimatorController controller in controllers) {
-        //    _animControllers.Add(controller.name, controller);
-        //}
-
-        //Room[] rooms = Resources.LoadAll<Room>("Rooms");
-        //foreach (Room room in rooms) {
-        //    _rooms.Add(room.Key, room);
-        //}
-
-        //AudioClip[] clips = Resources.LoadAll<AudioClip>("Sounds");
-        //foreach (AudioClip clip in clips) _clips.Add(clip.name, clip);
-
-        // ======
-
         if (_isInitialized) return;
         _isInitialized = true;
         LoadResource<Sprite>("Sprites");
@@ -61,14 +22,6 @@ public class ResourceManager {
     private void LoadResource<T>(string path, Func<T, string> keyFinder = null) where T : Object {
         keyFinder ??= x => x.name;
         _resources[typeof(T)] = Resources.LoadAll<T>(path).ToDictionary(keyFinder, x => (Object)x);
-        //Dictionary<string, Object> dictionary;
-        //if (!_resources.TryGetValue(typeof(T), out dictionary)) {
-        //    dictionary = new();
-        //    _resources[typeof(T)] = dictionary;
-        //}
-        //T[] objs = Resources.LoadAll<T>(path);
-        //foreach (T obj in objs)
-        //    dictionary.Add(keyFinder != null ? keyFinder(obj) : obj.name, obj);
     }
     public bool IsExist<T>(string key) where T : Object {
         if (!_resources.TryGetValue(typeof(T), out Dictionary<string, Object> dictionary)) return false;
@@ -108,68 +61,4 @@ public class ResourceManager {
         if (Main.Pool.Push(obj)) return;
         Object.Destroy(obj);
     }
-
-
-
-    //// 오브젝트가 풀 안에 있는지 없는지 확인 후 생성.
-    //public GameObject Instantiate(string key, Transform parent = null, bool pooling = false) {
-    //    GameObject prefab = LoadPrefab(key);
-    //    if (prefab == null) {
-    //        Debug.LogError($"[ResourceManager] Instantiate({key}): Failed to load prefab.");
-    //        return null;
-    //    }
-    //    if (pooling) return Main.Pool.Pop(prefab);
-    //    GameObject obj = GameObject.Instantiate(prefab, parent);
-    //    obj.name = prefab.name;
-    //    return obj;
-    //}
-
-    // 필요없는 오브젝트 파괴.
-
-    //// 리소스가 있는지 확인.
-    //public bool IsExistPrefab(string key) => _prefabs.ContainsKey(key);
-    //public bool IsExistAudioClip(string key) => _clips.ContainsKey(key);
-    //public GameObject LoadPrefab(string key) {
-    //    if (!_prefabs.TryGetValue(key, out GameObject prefab)) {
-    //        Debug.LogError($"[ResourceManager] LoadPrefab({key}): Failed to load prefab.");
-    //        return null;
-    //    }
-    //    return prefab;
-    //}
-    //public Sprite LoadSprite(string key) {
-    //    if (!_sprites.TryGetValue(key, out Sprite sprite)) {
-    //        Debug.LogError($"[ResourceManager] LoadSprite({key}): Failed to load sprite.");
-    //        return null;
-    //    }
-    //    return sprite;
-    //}
-    //public TextAsset LoadJsonData(string key) {
-    //    if (!_jsonData.TryGetValue(key, out TextAsset data)) {
-    //        Debug.LogError($"[ResourceManager] LoadJsonData({key}): Failed to load jsonData.");
-    //        return null;
-    //    }
-    //    return data;
-    //}
-    //public RuntimeAnimatorController LoadAnimController(string key) {
-    //    if (!_animControllers.TryGetValue(key, out RuntimeAnimatorController controller)) {
-    //        Debug.LogError($"[ResourceManager] LoadAnimController({key}): Failed to load animController.");
-    //        return null;
-    //    }
-    //    return controller;
-    //}
-    //public AudioClip LoadAudioClip(string key) {
-    //    if (!_clips.TryGetValue(key, out AudioClip clip)) {
-    //        Debug.LogError($"[ResourceManager] LoadAudioClip({key}): Failed to load AudioClip.");
-    //        return null;
-    //    }
-    //    return clip;
-    //}
-    //#region Load Rooms
-
-    //public List<Room> LoadRoom(RoomDirection direction) {
-    //    if ((int)direction == -1) direction = (RoomDirection)15;
-    //    return _rooms.Values.Where(r => r.Direction == direction).ToList();
-    //}
-
-    //#endregion
 }
